a67688e87b17d04f819f0a4735c2c13a6353a5e7
Khaled Muhamed <khaled.mhm3d2006@gmail.com>
2025-12-12 08:31:11 +0200

    Update traversal.cpp

--- BFS/traversal.cpp
+++ BFS/traversal.cpp
@@ -19,39 +19,43 @@ int dx_all[8] = { 1, 0, -1, 0, 1, 1, -1, -1 };
 int dy_all[8] = { 0, 1, 0, -1, -1, 1, -1, 1 };
 const int N = 1e5 + 5, M = 2e5 + 5, OO = 0x3f3f3f3f;
 
-int n, m, u, v;
-
+int n, m;
 vector<int> adj[N];
-
 int dis[N];
-void BFS(int src) { // O(n + m)
-    dis[src] = 0;
+
+void BFS(int src) {
+    // safer initialization: fill with OO
+    fill(dis, dis + n, OO);   // فقط عناصر 0..n-1
     queue<int> q;
+    dis[src] = 0;
     q.push(src);
 
     while (!q.empty()) {
-        int u = q.front();
-        q.pop();
+        int u = q.front(); q.pop();
         for (int v : adj[u]) {
             if (dis[v] == OO) {
-                q.push(v);
                 dis[v] = dis[u] + 1;
+                q.push(v);
             }
         }
     }
 }
+
 void TheManWhoNeverGivesUp() {
 
     cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        cin >> u >> v;
+    for (int i = 0; i < n; ++i) adj[i].clear();
+
+    for (int i = 0; i < m; ++i) {
+        int u, v; cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    memset(dis, OO, sizeof dis);
+
     BFS(0);
-    for (int u = 0; u < n; u++) {
-        cout << u << ' ' << dis[u] << el;
+
+    for (int u = 0; u < n; ++u) {
+        cout << u << ' ' << dis[u] << '\n';
     }
 }
 signed main() {
@@ -72,3 +76,31 @@ signed main() {
         TheManWhoNeverGivesUp();
     }
 }
+/*
+Input
+9 13
+0 1
+0 2
+0 3
+0 7
+1 6
+2 6
+3 5
+7 8
+7 6
+6 4
+5 6
+5 8
+6 8
+
+Output
+0 0
+1 1
+2 1
+3 1
+4 3
+5 2
+6 2
+7 1
+8 2
+*/
